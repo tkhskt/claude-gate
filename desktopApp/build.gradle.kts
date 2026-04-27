02d7b110b02ede578ca29a4f586de15c6c698cfb
tkhskt <luckymortality@gmail.com>
2026-04-27 09:40:16 +0900

    リネーム

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -29,7 +29,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "claude-gate"
+            packageName = "Claude Gate"
             packageVersion = "1.0.0"
 
             linux {
